213cbf7d372e6cc7102215f293350534f5cd227a
Alberto Jauregui <bastertwo@gmail.com>
2018-05-17 19:57:23 -0600

    Validate is event is full with filters

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy, :register]
+  before_action :set_event, only: [:show, :edit, :update, :destroy, :register, :register_user]
+  before_action :is_event_full?, only: :register_user
 
   # GET /events
   # GET /events.json
@@ -86,5 +87,11 @@ class EventsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:event).permit(:name, :start_date, :end_date, :max_students, :description)
+    end
+
+    def is_event_full?
+      if @event.is_event_full?
+        redirect_to register_to_event_path(@event), notice: "El evento #{@event.name} esta lleno"
+      end
     end
 end
